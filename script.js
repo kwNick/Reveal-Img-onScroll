fe3ea9731234f624514a68a5130a2031d1ff09cf
Nickolas Piraino <kwnp17c@gmail.com>
2024-05-07 16:42:55 -0400

    creating the javscript it doesnt seem to animate on scroll instead just on start

---
+++ script.js
@@ -0,0 +1,57 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+const bgColors = [
+    "#bb2a26",
+    "#7e7d65",
+    "#989682",
+    "#5e4076",
+];
+const bgColorElement = document.querySelector(".bg-color");
+
+gsap.utils.toArray(".item").forEach((item, index) => {
+    let img = item.querySelector(".item-img img");
+    gsap.fromTo(
+        img,
+        {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)"
+        },
+        {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            ease: "power1.out",
+            duration: 2,
+            ScrollTrigger: {
+                trigger: item,
+                start: "center bottom",
+                end: "bottom top",
+                // markers: true,
+                // scrub: true,
+                toggleActions: "play none none none",
+                onEnter: () => updateBackground(bgColors[index]),
+                onEnterBack: () => updateBackground(bgColors[index]),
+            },
+        },
+    )
+
+});
+
+function updateBackground(color) {
+    gsap.to(bgColorElement, {
+        background: `linear-gradient(0deg, ${color} 0%, rgba(252, 176, 69, 0), 100%)`,
+        duration: 2,
+        ease: "power1.out",
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const counterElement = document.querySelector(".counter p");
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+    function updateScrollPercentage() {
+        const scrollPosition = window.scrollY;
+        const scrolledPercentage = Math.round((scrollPosition / docHeight) * 100);
+        counterElement.textContent = `${scrolledPercentage}`;
+    }
+
+    window.addEventListener("scroll", updateScrollPercentage);
+});
